b6580cdb4c7616212b97de73d460912a642af202
Moises Lopez <moylop260@vauxoo.com>
2014-07-27 19:09:17 -0500

    [REF][runbot_prebuild] Add structure of directories in checkout

--- runbot_prebuild/runbot_prebuild.py
+++ runbot_prebuild/runbot_prebuild.py
@@ -1,17 +1,28 @@
 # -*- encoding: utf-8 -*-
 #TODO: license from vauxoo
 from openerp.osv import fields, osv
+import os
+import shutil
+import logging
+import glob
+
+_logger = logging.getLogger(__name__)
+
+def mkdirs(dirs):
+    for d in dirs:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
 class runbot_prebuild_branch(osv.osv):
     _name = "runbot.prebuild.branch"
     _rec_name = 'branch_id'
     
     _columns = {
-        'branch_id': fields.many2one('runbot.branch', 'Branch depends', required=True, 
+        'branch_id': fields.many2one('runbot.branch', 'Modules branch', required=True, 
             ondelete='cascade', select=1),
-        'check_pr': fields.boolean('Check Pull Request to this branch?', 
-            help='If is True, this will check PR for this branch in this prebuild'),
-        'revision_id': fields.char('SHA commit', 
+        'check_pr': fields.boolean('Check PR', 
+            help='If is True, this will check Pull Request for this branch in this prebuild'),
+        'sha_commit': fields.char('SHA commit', size=40,
             help='Empty=Currently version\nSHA=Get this version in builds'),
         'prebuild_id': fields.many2one('runbot.prebuild', 'Pre-Build', required=True, 
             ondelete='cascade', select=1),
@@ -27,9 +38,9 @@ class runbot_prebuild(osv.osv):
         'name': fields.char("Name", size=128, required=True),
         'main_branch_id': fields.many2one('runbot.branch', 'Main branch', required=True, 
             ondelete='cascade', select=1),
-        'prebuild_branch_ids': fields.one2many('runbot.prebuild.branch', 'prebuild_id', 
-            string='Branches extra dependencies', copy=True,
-            help="Community addon branches which need to be present to run tests."),
+        'module_branch_ids': fields.one2many('runbot.prebuild.branch', 'prebuild_id', 
+            string='Branches of modules', copy=True,
+            help="Community addons branches which need to run tests."),
         'repo_id': fields.related('main_branch_id', 'repo_id', type="many2one", 
             relation="runbot.repo", string="Repository from main branch", readonly=True, 
             store=True, ondelete='cascade', select=1),
@@ -49,7 +60,58 @@ class runbot_prebuild(osv.osv):
             help="Script to execute after run build"),
     }
 
-class runbot_build(osv.osv):
-    _name = "runbot.branch"
+class runbot_branch(osv.osv):
+    _inherit = "runbot.branch"
     #TODO: get_name -> branch.repo_id.name + branch.name
     #TODO: name search -> branch.repo_id.name + branch.name
+
+class runbot_build(osv.osv):
+    _inherit = "runbot.build"
+
+    _columns = {
+        'prebuild_id': fields.many2one('runbot.prebuild', string='Runbot Pre-Build', 
+            required=False, help="This is the origin of instance data."),
+    }
+    
+    def checkout_params(self, cr, uid, ids, main_branch_id, module_branch_ids, commit_sha=None, context=None):
+        branch_obj = self.pool.get('runbot.branch')
+        main_branch = branch_obj.browse(cr, uid, [main_branch_id], context=context)[0]
+        for build in self.browse(cr, uid, ids, context=context):
+            # starts from scratch
+            if os.path.isdir(build.path()):
+                shutil.rmtree(build.path())
+            _logger.debug('Creating build in path "%s"'%( build.path() ))
+            
+            # runbot log path
+            mkdirs([build.path("logs"), build.server('addons')])
+
+            # checkout main branch
+            main_branch.repo_id.git_export(commit_sha or main_branch.name, build.path())
+
+            # TODO use git log to get commit message date and author
+
+            # v6 rename bin -> openerp
+            if os.path.isdir(build.path('bin/addons')):
+                shutil.move(build.path('bin'), build.server())
+
+            # move all addons to server addons path
+            for module in glob.glob( build.path('addons/*') ):
+                shutil.move(module, build.server('addons'))
+            
+            if module_branch_ids:
+                for module_branch in branch_obj.browse(cr, uid, module_branch_ids, context=context):
+                    module_branch.repo_id.git_export(commit_sha or module_branch.name, build.server("addons"))
+                    #Note: If a module name is duplicate no make error. TODO: But is good make info.
+
+    
+    def checkout(self, cr, uid, ids, context=None):
+        for build in self.browse(cr, uid, ids, context=context):
+            build.prebuild_id = self.pool.get('runbot.prebuild').browse(cr, uid, [4], context=context)[0]#Delete this line, only is for test
+            if not build.prebuild_id:
+                #TODO: Split branches for use same function "checkout_params"
+                return super(runbot_build, self).checkout(cr, uid, ids, context=context)
+            else:
+                main_branch_id = build.prebuild_id.main_branch_id.id
+                module_branch_ids = [module_branch_id.branch_id.id for module_branch_id in build.prebuild_id.module_branch_ids]
+                self.checkout_params(cr, uid, [build.id], main_branch_id=main_branch_id, module_branch_ids=module_branch_ids, commit_sha=build.prebuild_id, context=context)
+                
