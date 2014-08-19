70e2d31b24151a6a74598fa95c255615c5e7946c
Luis Torres <luis_t@vauxoo.com>
2014-08-19 18:02:39 -0500

    [IMP][runbot_prebuild]Was added field to save the full name of branch, based in your name and name of your repo

--- runbot_prebuild/runbot_prebuild.py
+++ runbot_prebuild/runbot_prebuild.py
@@ -268,6 +268,9 @@ class runbot_prebuild(osv.osv):
         return build_ids
 
 class runbot_branch(osv.osv):
+    '''
+    The inherit of this class is used to add the field full_name in the model
+    '''
     _inherit = "runbot.branch"
 
     def name_get(self, cr, uid, ids, context=None):
@@ -282,6 +285,75 @@ class runbot_branch(osv.osv):
             name = '[' + record[1] + '] ' + branch.repo_id.name
             res.append((record[0], name))
         return res
+    
+    def name_search(self, cr, uid, name, args=None, operator='ilike',
+        context=None, limit=100):
+        '''
+        This method added in the many2one to this model that can search
+        by full_name.
+        '''
+        if not args:
+            args = []
+        if not context:
+            context = {}
+        ids = []
+        res = super(runbot_branch, self).name_search(cr, uid, name, args=args,
+            operator=operator, context=context, limit=limit)
+        for element in res:
+            ids.append(element[0])
+        if name:
+            ids2 = self.search(cr, uid, [('full_name', operator, name)] + args,
+                limit=limit, context=context)
+            ids.extend( ids2 )
+        ids = list( set ( ids ))
+        return self.name_get(cr, uid, ids, context)
+    
+    def _get_branch_fullname(self, cr, uid, ids, name, args, context=None):
+        '''
+        This method is used to load data in full_name field with the next
+            format:
+            [branch.name] branch.repo_id.name
+        '''
+        if context is None:
+            context = {}
+        res = {}
+        for branch in self.browse(cr, uid, ids, context=context):
+            res[branch.id] = '[' + ( branch.name or '' )+ '] ' + (
+                branch.repo_id and branch.repo_id.name or '' )
+        return res
+        
+    def _get_name_repo(self, cr, uid, ids, context=None):
+        '''
+        This method is used to update data in full_name field with the next
+            format when the name of repo is changed:
+            [branch.name] branch.repo_id.name
+        '''
+        if context is None:
+            context = {}
+        res = {}
+        branch_obj = self.pool.get('runbot.branch')
+        for repo in self.pool.get('runbot.repo').browse(cr, uid, ids,
+            context=context):
+            branch_ids = branch_obj.search(cr, uid, [
+                ('repo_id', '=', repo.id), ], context=context)
+            for branch in branch_obj.browse(cr, uid, branch_ids,
+                context=context):
+                res[branch.id] = '[' + ( branch.name or '' )+ '] ' + (
+                    repo.name or '' )
+        branch_ids = []
+        if res:
+            branch_ids = branch_obj.search(cr, uid, [
+                ('repo_id', 'in', res.keys())], context=context)
+        return branch_ids
+        
+    _columns = {
+        'full_name': fields.function(_get_branch_fullname, string='Full name',
+            type='char',
+            store={
+                'runbot.branch': (lambda self, cr, uid, ids, c={}: ids, [], 50),
+                'runbot.repo': (_get_name_repo, None, 50),
+                })
+    }
 
 class runbot_build(osv.osv):
     _inherit = "runbot.build"
