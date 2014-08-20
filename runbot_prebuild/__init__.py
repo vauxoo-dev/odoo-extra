101461c32f16f24d59644a800df0fec24ecda598
Luis Torres <luis_t@vauxoo.com>
2014-08-20 17:29:16 -0500

    [REF][runbot_prebuil]Was divided the file in two, complete documentation

--- runbot_prebuild/__init__.py
+++ runbot_prebuild/__init__.py
@@ -23,4 +23,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import runbot
 import runbot_prebuild
+
